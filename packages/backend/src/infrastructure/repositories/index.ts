a7d772e7968d7e676464ed578a0e7ef05c1ee285
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2026-01-23 22:28:00 -0300

    chore(02-04): export all repository factory functions
    - Create repositories/index.ts barrel file for DI
    - Export createPlanRepository, createSubscriptionRepository
    - Export createAthleteRepository, createAthleteInvitationRepository

---
+++ packages/backend/src/infrastructure/repositories/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Repository Factory Exports
+ *
+ * EXCEPTION: This barrel file is allowed per architecture.md
+ * as it aggregates repository factories for dependency injection.
+ */
+
+export { createPlanRepository } from "./plan.repository";
+export { createSubscriptionRepository } from "./subscription.repository";
+export { createAthleteRepository } from "./athlete.repository";
+export { createAthleteInvitationRepository } from "./athlete-invitation.repository";
