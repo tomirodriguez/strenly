8ff4c643cf1359556398c02b67591a05c960b8f1
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2026-01-26 22:18:54 -0300

    fix(03.4-08): fix Base UI button component warning
    - Add nativeButton={false} to DropdownMenuTrigger
    - Suppresses warning when trigger wraps non-native button elements

--- apps/coach-web/src/components/ui/dropdown-menu.tsx
+++ apps/coach-web/src/components/ui/dropdown-menu.tsx
@@ -14,7 +14,7 @@ function DropdownMenuPortal({ ...props }: MenuPrimitive.Portal.Props) {
 }
 
 function DropdownMenuTrigger({ ...props }: MenuPrimitive.Trigger.Props) {
-  return <MenuPrimitive.Trigger data-slot="dropdown-menu-trigger" {...props} />
+  return <MenuPrimitive.Trigger data-slot="dropdown-menu-trigger" nativeButton={false} {...props} />
 }
 
 function DropdownMenuContent({
